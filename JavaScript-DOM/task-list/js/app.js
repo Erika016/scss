432d2c42b7254b2d92f590b8d6af3f91bb1b04c4
erika <ericadominguezbarrientos@gmail.com>
2022-04-17 20:10:27 +0200

    he añadido el boton filtro con 3 opciones solicitadas

--- JavaScript-DOM/task-list/js/app.js
+++ JavaScript-DOM/task-list/js/app.js
@@ -4,12 +4,14 @@ window.addEventListener("load", () => {
   let text = "";
   let alert = document.querySelector(".alert");
 
+  // función para alerta de elemento cercano
   //function for close element alert
   let close = alert.firstElementChild;
   close.addEventListener("click", () => {
     alert.classList.add("dismissible");
   });
 
+  // función para obtener entrada de valor en foco
   //function for get value input on focus
   let input = document.querySelector("input");
   input.addEventListener("focus", () => {
@@ -20,6 +22,7 @@ window.addEventListener("load", () => {
     });
   });
 
+  // función donde se puede hacer clic en la flecha del elemento
   //function where element arrow is clickable
   let arrow = document.querySelector(".arrow");
   arrow.addEventListener("click", (event) => {
@@ -38,6 +41,7 @@ window.addEventListener("load", () => {
     }
   });
 
+  // funcion para completar una tarea
   //function for task complete
   let done = document.querySelectorAll(".fa-circle-check");
   done.forEach((item) => {
@@ -46,34 +50,36 @@ window.addEventListener("load", () => {
     });
   });
 
+  // poder escribir una tarea
   //Enable user to edit task
   let edit = document.querySelectorAll(".fa-pen");
-  edit.forEach((item)=>{
-    item.addEventListener("click", (event)=>{
-        editTask(event, false);
+  edit.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      editTask(event, false);
     });
   });
 
   let taskContent = document.querySelectorAll(".task");
-  taskContent.forEach((item)=>{
-    item.addEventListener("focus", (event)=>{
-        editTask(event, true);
+  taskContent.forEach((item) => {
+    item.addEventListener("focus", (event) => {
+      editTask(event, true);
     });
 
-    item.addEventListener("blur", (event)=>{
-        event.target.classList.remove("editable");
-    })
+    item.addEventListener("blur", (event) => {
+      event.target.classList.remove("editable");
+    });
   });
 
-  
+  // borrar contenido
   let trash = document.querySelectorAll(".fa-trash");
-  trash.forEach((item)=>{
-    item.addEventListener("click", (event)=>{
-        removeRow(event, false);
+  trash.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      removeRow(event, false);
     });
   });
 });
 
+// crear una nueva fila
 //function to create new row
 const generateRow = (id, text) => {
   let newRow = document.createElement("tr");
@@ -97,29 +103,45 @@ const generateRow = (id, text) => {
     </td>
     `;
 
-    //Click icon check
-    newRow.firstElementChild.firstElementChild.addEventListener("click", (event)=>{
-        deleteTask(event);
-    });
+  // Haga clic en el icono de verificación
+  //Click icon check
+  newRow.firstElementChild.firstElementChild.addEventListener(
+    "click",
+    (event) => {
+      deleteTask(event);
+    }
+  );
 
-    //Over text
-    newRow.firstElementChild.lastElementChild.addEventListener("click", (event)=>{
-        editTask(event,true);
-    });
+  // sobre texto
+  //Over text
+  newRow.firstElementChild.lastElementChild.addEventListener(
+    "click",
+    (event) => {
+      editTask(event, true);
+    }
+  );
 
-    //Icon pen
-    newRow.firstElementChild.nextElementSibling.firstElementChild.addEventListener("click", (event)=>{
-        editTask(event,false);
-    });
+  //Icon pen
+  newRow.firstElementChild.nextElementSibling.firstElementChild.addEventListener(
+    "click",
+    (event) => {
+      editTask(event, false);
+    }
+  );
 
-    //Icon trash
-    newRow.lastElementChild.firstElementChild.addEventListener("click", (event)=>{
-        removeRow(event, false);
-    });
+  // Bolígrafo icono
+  //Icon trash
+  newRow.lastElementChild.firstElementChild.addEventListener(
+    "click",
+    (event) => {
+      removeRow(event, false);
+    }
+  );
 
   return newRow;
 };
 
+// función para completar la tarea
 //function to complete task
 const deleteTask = (event) => {
   let task = event.target.nextElementSibling;
@@ -134,37 +156,50 @@ const deleteTask = (event) => {
   }
 };
 
+// función para editar tarea
 //function to edit task
-const editTask=(event, onFocus)=>{
-    if(onFocus===true){
-        let editable = event;
-        event.target.classList.add("editable");
-        document.addEventListener('keydown', (event)=>{
-            console.log(event.key);
-            if(event.key==="Escape"){
-                if(editable.target.innerHTML.trim()===""){
-                    removeRow(editable, true);
-                }
-                event.target.classList.remove("editable");
-                editable.target.blur();        
-            }
-        })
-    }else{
-        let editable = event.target.parentNode.parentNode.previousElementSibling.lastElementChild;
-        editable.classList.add("editable");
-        editable.focus();
-    }
-}
-
+const editTask = (event, onFocus) => {
+  if (onFocus === true) {
+    let editable = event;
+    event.target.classList.add("editable");
+    document.addEventListener("keydown", (event) => {
+      console.log(event.key);
+      if (event.key === "Escape") {
+        if (editable.target.innerHTML.trim() === "") {
+          removeRow(editable, true);
+        }
+        event.target.classList.remove("editable");
+        editable.target.blur();
+      }
+    });
+  } else {
+    let editable =
+      event.target.parentNode.parentNode.previousElementSibling
+        .lastElementChild;
+    editable.classList.add("editable");
+    editable.focus();
+  }
+};
+// Función para eliminar fila
 //Function to remove row
-const removeRow = (event, editing)=>{
-    if(editing){
-        //remove when value == ""
-        event.target.parentNode.parentNode.remove();
-    }else{
-        //remove when click icon delete
-        event.target.parentNode.parentNode.parentNode.remove();
-    }
-}
+const removeRow = (event, editing) => {
+  if (editing) {
+    // eliminar cuando el valor == ""
+    //remove when value == ""
+    event.target.parentNode.parentNode.remove();
+  } else {
+    // //eliminar al hacer clic en el icono eliminar
+    //remove when click icon delete
+    event.target.parentNode.parentNode.parentNode.remove();
+  }
+};
 
 //hacer un filtro de busqueda con campo select donde exista las siguientes opciones All (por defecto), done, undone, segun la opcion seleccionada debe de aparecer las filas correspondiente de cada uno
+
+// boton para filtrar
+
+const todoHecho= document.querySelector('todo');
+todoHecho.addEventListener('click', muestraTodo);
+
+const muestraTodo= editable.filter(x => editTask == true)
+console.log(muestraTodo);
